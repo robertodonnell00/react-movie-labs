f42d6df5f5f65c8b882961ccffdc33b35b88ed8a
robertodonnell00 <robertodonnell76@yahoo.ie>
2025-09-30 10:02:21 +0100

    Fetching genre list from API.

--- movies/src/components/filterMoviesCard/index.jsx
+++ movies/src/components/filterMoviesCard/index.jsx
@@ -10,6 +10,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
+import React, {useState, useEffect}  from "react";
 
 const formControl = 
   {
@@ -20,11 +21,35 @@ const formControl =
 
 export default function FilterMoviesCard(props) {
 
-  const genres = [
-    {id: 1, name: "Animation"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Thriller"}
-  ]
+  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+
+      useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        import.meta.env.VITE_TMDB_KEY
+    )
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json.genres) 
+        return json.genres
+      })
+      .then(apiGenres => {
+        setGenres([genres[0], ...apiGenres]);
+      });
+      // eslint-disable-next-line
+  }, []);
+
+  const handleChange = (e, type, value) => {
+    e.preventDefault()
+    // Completed later
+  };
+  const handleTextChange = e => {
+    handleChange(e, "name", e.target.value)
+  }
+  const handleGenreChange = e => {
+    handleChange(e, "genre", e.target.value)
+  };
+
 
   return (
     <Card 
@@ -37,19 +62,24 @@ export default function FilterMoviesCard(props) {
           <SearchIcon fontSize="large" />
           Filter the movies.
         </Typography>
-        <TextField
-          sx={{...formControl}}
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
+            <TextField
+                sx={{...formControl}}
+                id="filled-search"
+                label="Search field"
+                type="search"
+                variant="filled"
+                value={props.titleFilter}
+                onChange={handleTextChange}
+                />
         <FormControl sx={{...formControl}}>
           <InputLabel id="genre-label">Genre</InputLabel>
-          <Select
-            labelId="genre-label"
-            id="genre-select"
-          >
+            <Select
+                labelId="genre-label"
+                id="genre-select"
+                defaultValue=""
+                value={props.genreFilter}
+                onChange={handleGenreChange}
+            >
             {genres.map((genre) => {
               return (
                 <MenuItem key={genre.id} value={genre.id}>
