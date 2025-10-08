d69af502ccb0d3c9e091ba835f2a44ac43c9c1e5
robertodonnell00 <robertodonnell76@yahoo.ie>
2025-10-08 10:09:04 +0100

    Fix backward/forward arrows. Bug: screen not displaying

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -9,15 +9,15 @@ import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
-        <BrowserRouter>
+    <BrowserRouter>
       <SiteHeader />
-      <Routes>
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-      </Routes>
+        <Routes>
+          <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+          <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
     </BrowserRouter>
   );
 };
